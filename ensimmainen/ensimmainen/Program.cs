8167c7d1aecf4d49d333b1bf3e4c429d1cce9ba8
mika huotari <mika.huotari@edu.keuda.fi>
2023-01-13 11:05:35 +0200

    grud

---
+++ ensimmainen/ensimmainen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Ensimmäinen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei vaan Käyttäjä");
+            Console.Write("Antaisitteko Nimenne: ");
+            string name = Console.ReadLine();
+            Console.Write("Hyvää Päivää, ");
+            Console.Write(name);
+            Console.Write();
+            Console.Write("Hyvä voitte poistua Enterillä");
+            Console.ReadLine();
+        }
+        }
+}
