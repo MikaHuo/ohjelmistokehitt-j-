0e60f4a2c5669e59faea2a50588745f7dd508d7b
mika huotari <mika.huotari@edu.keuda.fi>
2023-01-30 13:07:19 +0200

    push 1 ma koulu

---
+++ kolmas teht1/teht 14/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int luku1, luku2;
+        
+        Console.WriteLine("Onko lukema positiivinen vai negatiivinen:");
+    paluu:
+        try
+        {
+            Console.Write(" Anna ensimmäinen luku: ");
+           luku1 = int.Parse(Console.ReadLine());
+            Console.Write(" Anna toinen luku: ");
+            luku2= int.Parse(Console.ReadLine());
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.ToString());
+            Console.Write("Et antnut kokonais lukua! ");
+            goto paluu;
+        }
+        if (luku1 > 0)
+        {
+            Console.WriteLine(luku1 + " luku1 on positiivinen");
+        }
+        else
+        {
+            Console.WriteLine(luku1 + " Luku1 on negatiivinen");
+        }
+        if (luku2 > 0)
+        {
+            Console.WriteLine(luku2 + " Luku2 on positiivinen");
+        }
+        else 
+        {
+            Console.WriteLine(luku2 + " Luku2 on negatiivinen");
+
+        }
+    }
+}
---
+++ omia jtn/odd even/odd even/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int luku;
+        Console.WriteLine("Selvitä onko luku parillinen vai pariton");
+        paluu:
+        try
+        {
+            luku = int.Parse(Console.ReadLine());
+        }
+        catch (Exception ex)        
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine(" Antamasi luku ei ollut kokonais luku! Tarkasta syöte! ");
+           goto paluu;
+        }
+        switch (luku % 2) 
+        {
+            case 0: Console.WriteLine(" Antamasi luku on PARILLINEN");
+                break;
+            case 1: Console.WriteLine(" Antamsi luku on PARITON");
+                break;
+        }
+             
+    }
+}
